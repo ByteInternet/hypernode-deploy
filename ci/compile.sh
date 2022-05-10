f56c89a5f45e96ee14599def33fdbfd98bdc9d12
Timon de Groot <timon.degroot@hypernode.com>
2022-05-10 13:58:20 +0200

    compile.sh: run composer non interactively

--- ci/compile.sh
+++ ci/compile.sh
@@ -4,11 +4,11 @@ set -e
 cd "$(dirname "$0")/../"
 
 # Install dependencies
-composer install --no-dev --optimize-autoloader --ignore-platform-reqs
+composer install --no-dev --optimize-autoloader --ignore-platform-reqs --no-interaction
 
 # Install build tool
 cd build
-composer install --no-dev --optimize-autoloader --ignore-platform-reqs
+composer install --no-dev --optimize-autoloader --ignore-platform-reqs --no-interaction
 cd ../
 
 # Run build
