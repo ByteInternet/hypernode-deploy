bad02eecf6148a25a9891e4e6a976258df54ae69
Timon de Groot <timon.degroot@hypernode.com>
2022-05-18 12:38:49 +0200

    Update Dockerfile

--- ci/compile.sh
+++ ci/compile.sh
@@ -3,12 +3,9 @@ set -e
 
 cd "$(dirname "$0")/../"
 
-# Install dependencies
-composer install --no-dev --optimize-autoloader --ignore-platform-reqs --no-interaction
-
 # Install build tool
 cd build
-composer install --no-dev --optimize-autoloader --ignore-platform-reqs --no-interaction
+composer install --no-dev --optimize-autoloader
 cd ../
 
 # Run build
