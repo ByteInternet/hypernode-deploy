8dd6a36912fb55430b1305054ee12762316597a1
Alexander Grooff <alexandergrooff@gmail.com>
2022-07-18 17:14:21 +0200

    test: setup magento install

--- runtests.sh
+++ runtests.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 # Clear up env
-# trap "docker-compose down -v" EXIT
+trap "docker-compose down -v" EXIT
 
 # Start hypernode-docker
 docker-compose up -d hypernode
@@ -13,11 +13,32 @@ docker-compose up -d hypernode
 if [ ! -e "../magento2.komkommer.store/build" ]; then
     docker-compose run deploy hypernode-deploy build
 else
-    echo "Vendor folder already exists, skipping build"
+    echo "Build folder already exists, skipping build"
 fi
 
-# Prepare Hypernode env
-docker-compose exec hypernode mkdir -p /data/web/apps/magento2.komkommer.store
+# Prepare env
+rm ../magento2.komkommer.store/app/etc/env.php || /bin/true
+echo "Waiting for MySQL to be available on the Hypernode container"
+docker-compose exec hypernode bash -c "until mysql -e 'select 1' 2> /dev/null ; do sleep 1; done"
+docker-compose exec hypernode mkdir -p /data/web/apps/magento2.komkommer.store/shared/app/etc/
+
+# You need a working Magento install before you can use the hn-deploy
+# This sets up the database on the Hypernode container and generates a valid env.php
+docker-compose exec hypernode mysql -e "CREATE DATABASE dummytag_preinstalled_magento"
+function install_magento() {
+    docker-compose exec hypernode bash -c "/banaan/bin/magento setup:install  \
+    --base-url=http://magento2.komkommer.store  \
+    --db-host=mysqlmaster.dummytag.hypernode.io  \
+    --db-name=dummytag_preinstalled_magento --db-user=app  \
+    --db-password=RjeXDj7FAPWCyBxlWEAbKMSkpKyxMptY  \
+    --admin-firstname=admin --admin-lastname=admin  \
+    --admin-email=admin@admin.com --admin-user=admin  \
+    --admin-password=admin123 --language=en_US --currency=USD  \
+    --timezone=America/Chicago --elasticsearch-host=localhost"
+}
+install_magento || /bin/true
+install_magento  # Second time works?
+docker-compose exec hypernode cp /banaan/app/etc/env.php /data/web/apps/magento2.komkommer.store/shared/app/etc/env.php
 docker-compose exec hypernode chown -R app:app /data/web/apps/magento2.komkommer.store
 
 echo "Waiting for SSH to be available on the Hypernode container"
@@ -25,4 +46,4 @@ docker-compose run deploy bash -c "until ssh app@hypernode echo UP! 2> /dev/null
 
 # SSH from deploy container to hypernode container
 chmod 0600 ci/test/.ssh/id_rsa
-docker-compose run deploy hypernode-deploy deploy production -vvv -f /web/deploy_simple.php
+docker-compose run deploy hypernode-deploy deploy production -f /web/deploy_simple.php
