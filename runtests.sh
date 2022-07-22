210fe8ec12a8ee93bc8e4a11f80a6d8aac0f658f
Alexander Grooff <alexandergrooff@gmail.com>
2022-07-22 13:41:15 +0200

    ci: dont wait for ssh

--- runtests.sh
+++ runtests.sh
@@ -46,7 +46,7 @@ $HN cat /root/.ssh/authorized_keys
 $DP ssh-keygen -y -f /root/.ssh/id_rsa
 $DP stat /root/.ssh/id_rsa
 # Allocate pseudotty because github actions doesn't support TTY
-$DP bash -c "until ssh -t -v -i /root/.ssh/id_rsa root@hypernode echo UP! ; do sleep 1; done"
+# $DP bash -c "until ssh -t -v -i /root/.ssh/id_rsa root@hypernode echo UP! ; do sleep 1; done"
 $DP rsync -e "ssh -t" -a hypernode:/data/web/magento2/ /web
 $DP rm /web/app/etc/env.php
 
