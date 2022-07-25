485897dab612edfaf34826e1c3ff9948cee0d715
Timon de Groot <timon.degroot@hypernode.com>
2022-07-25 17:19:45 +0200

    ci: comply with spellcheck for release_semantic_versions.sh

--- ci/release_semantic_versions.sh
+++ ci/release_semantic_versions.sh
@@ -9,7 +9,7 @@ if [ ! -n "${INPUT_VERSION}" ]; then
     exit 1
 fi
 
-if echo $INPUT_VERSION | egrep -q "(dev|rc|alpha|beta)"; then
+if echo "${INPUT_VERSION}" | grep -E -q "(dev|rc|alpha|beta)"; then
     echo "Not publishing short semantic versions for dev, rc, alpha or beta tag"
     exit 0
 fi
@@ -21,15 +21,15 @@ function tag_and_publish () {
     docker push "${TARGET_TAG}"
 }
 
-if echo $INPUT_VERSION | grep -q "\."; then
-    MAJOR_VERSION=$(echo $INPUT_VERSION | cut -d. -f1)
-    MINOR_VERSION=$(echo $INPUT_VERSION | cut -d. -f2)
-    PATCH_VERSION=$(echo $INPUT_VERSION | cut -d. -f3)
+if echo "${INPUT_VERSION}" | grep -F "."; then
+    MAJOR_VERSION=$(echo "${INPUT_VERSION}" | cut -d. -f1)
+    MINOR_VERSION=$(echo "${INPUT_VERSION}" | cut -d. -f2)
+    PATCH_VERSION=$(echo "${INPUT_VERSION}" | cut -d. -f3)
 
-    if [ -n "$MINOR_VERSION" ] && [ -n "$PATCH_VERSION" ]; then
-        if echo $PATCH_VERSION | grep -q "-"; then
-            PATCH_SUFFIX=$(echo $PATCH_VERSION | cut -d- -f2-)
-            PATCH_VERSION=$(echo $PATCH_VERSION | cut -d- -f1)
+    if [ -n "${MINOR_VERSION}" ] && [ -n "${PATCH_VERSION}" ]; then
+        if echo "${PATCH_VERSION}" | grep -q "-"; then
+            #PATCH_SUFFIX=$(echo "${PATCH_VERSION}" | cut -d- -f2-)
+            PATCH_VERSION=$(echo "${PATCH_VERSION}" | cut -d- -f1)
         fi
         tag_and_publish "$IMAGE:$INPUT_VERSION-$TAG_SPECS" "$IMAGE:$MAJOR_VERSION.$MINOR_VERSION.$PATCH_VERSION-$TAG_SPECS"
     fi
