176f2d652fd279664655ff6197d8f4e9c30b7953
Alexander Grooff <alexandergrooff@gmail.com>
2022-08-15 17:39:38 +0200

    chore: linting

--- src/Deployer/Task/PlatformConfiguration/HypernodeSettingTask.php
+++ src/Deployer/Task/PlatformConfiguration/HypernodeSettingTask.php
@@ -3,15 +3,16 @@
 namespace Hypernode\Deploy\Deployer\Task\PlatformConfiguration;
 
 use Deployer\Task\Task;
-use function Deployer\after;
-use function Deployer\run;
-use function Deployer\task;
-
 use Hypernode\Deploy\Deployer\Task\ConfigurableTaskInterface;
 use Hypernode\Deploy\Deployer\Task\IncrementedTaskTrait;
 use Hypernode\Deploy\Deployer\Task\TaskBase;
 use Hypernode\DeployConfiguration\TaskConfigurationInterface;
 use Hypernode\DeployConfiguration\PlatformConfiguration\HypernodeSettingConfiguration;
+use Webmozart\Assert\Assert;
+
+use function Deployer\after;
+use function Deployer\run;
+use function Deployer\task;
 
 class HypernodeSettingTask extends TaskBase implements ConfigurableTaskInterface
 {
@@ -29,6 +30,7 @@ class HypernodeSettingTask extends TaskBase implements ConfigurableTaskInterface
 
     public function configureWithTaskConfig(TaskConfigurationInterface $config): ?Task
     {
+        Assert::isInstanceOf($config, HypernodeSettingConfiguration::class);
         $attribute = $config->getAttribute();
         $value = $config->getValue();
         $taskName = "deploy:hypernode:setting:{$attribute}";
