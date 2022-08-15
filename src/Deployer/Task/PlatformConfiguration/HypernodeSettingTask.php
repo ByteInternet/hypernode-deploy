37a0881aabd9b78ea7356c62000d89a90963dd5e
Alexander Grooff <alexandergrooff@gmail.com>
2022-08-15 17:31:37 +0200

    fix: use new task structure

--- src/Deployer/Task/PlatformConfiguration/HypernodeSettingTask.php
+++ src/Deployer/Task/PlatformConfiguration/HypernodeSettingTask.php
@@ -9,12 +9,11 @@ use function Deployer\task;
 
 use Hypernode\Deploy\Deployer\Task\ConfigurableTaskInterface;
 use Hypernode\Deploy\Deployer\Task\IncrementedTaskTrait;
-use Hypernode\Deploy\Deployer\Task\RegisterAfterInterface;
-use Hypernode\DeployConfiguration\Configuration;
+use Hypernode\Deploy\Deployer\Task\TaskBase;
 use Hypernode\DeployConfiguration\TaskConfigurationInterface;
 use Hypernode\DeployConfiguration\PlatformConfiguration\HypernodeSettingConfiguration;
 
-class HypernodeSettingTask implements ConfigurableTaskInterface, RegisterAfterInterface
+class HypernodeSettingTask extends TaskBase implements ConfigurableTaskInterface
 {
     use IncrementedTaskTrait;
 
@@ -23,41 +22,20 @@ class HypernodeSettingTask implements ConfigurableTaskInterface, RegisterAfterIn
         return 'deploy:configuration:hypernode:setting:';
     }
 
-    public function configureTask(TaskConfigurationInterface $config): void
-    {
-    }
-
     public function supports(TaskConfigurationInterface $config): bool
     {
         return $config instanceof HypernodeSettingConfiguration;
     }
 
-    public function registerAfter(): void
+    public function configureWithTaskConfig(TaskConfigurationInterface $config): ?Task
     {
-        after('deploy:setup', 'deploy:hypernode:setting');
-    }
-
-    /**
-     * @param TaskConfigurationInterface|HypernodeSettingConfiguration $config
-     */
-    public function build(TaskConfigurationInterface $config): ?Task
-    {
-        return null;
-    }
-
-    public function configure(Configuration $config): void
-    {
-        $tasks = [];
-        foreach ($config->getPlatformConfigurations() as $platformConfiguration) {
-            if ($platformConfiguration instanceof HypernodeSettingConfiguration) {
-                $attribute = $platformConfiguration->getAttribute();
-                $value = $platformConfiguration->getValue();
-                $task = task("deploy:hypernode:setting:{$attribute}", function () use ($attribute, $value) {
-                    run("hypernode-systemctl settings {$attribute} {$value}");
-                });
-                $tasks[] = $task->getName();
-            }
-        }
-        task('deploy:hypernode:setting', $tasks);
+        $attribute = $config->getAttribute();
+        $value = $config->getValue();
+        $taskName = "deploy:hypernode:setting:{$attribute}";
+        $task = task($taskName, function () use ($attribute, $value) {
+            run("hypernode-systemctl settings {$attribute} {$value}");
+        });
+        after('deploy:setup', $taskName);
+        return $task;
     }
 }
