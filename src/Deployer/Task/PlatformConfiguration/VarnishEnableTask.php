e7bf68f36815dc8503deebb7903fba6e0e96dc9c
Martijn <martijn@hipex.io>
2022-08-16 09:36:53 +0200

    prompt yes when kamikaze requesting for changes

--- src/Deployer/Task/PlatformConfiguration/VarnishEnableTask.php
+++ src/Deployer/Task/PlatformConfiguration/VarnishEnableTask.php
@@ -43,7 +43,7 @@ class VarnishEnableTask extends TaskBase implements ConfigurableTaskInterface
         } else {
             task(self::TASK_NAME, function () use ($config) {
                 run("hypernode-systemctl settings varnish_enabled True --block");
-                run("hypernode-systemctl settings varnish_version {$config->getVersion()} --block");
+                run("echo yes | hypernode-systemctl settings varnish_version {$config->getVersion()} --block");
                 run('hypernode-manage-vhosts {{domain}} --varnish');
             });
         }
