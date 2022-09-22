85e5c490ae7d6a276d564b107fd384790bec2615
Timon de Groot <timon.degroot@hypernode.com>
2022-09-22 09:24:56 +0200

    Fix missing deploy:prepare step
    This problem was introduced in #54

--- src/Deployer/Task/Deploy/UploadTask.php
+++ src/Deployer/Task/Deploy/UploadTask.php
@@ -30,7 +30,7 @@ class UploadTask extends TaskBase
         task('deploy:upload', [
             'deploy:info',
             'prepare:ssh',
-            'deploy:release',
+            'deploy:prepare',
             'deploy:copy',
             'deploy:deploy',
         ])->select("roles=$role");
