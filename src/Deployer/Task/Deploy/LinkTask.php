f5037d435498eb9feb6ded83d2ba84cea5a5dae7
SBernards <sbernards@emico.nl>
2023-10-06 10:31:06 +0200

    fix: revert release path change

--- src/Deployer/Task/Deploy/LinkTask.php
+++ src/Deployer/Task/Deploy/LinkTask.php
@@ -26,7 +26,7 @@ class LinkTask extends TaskBase
                     return;
                 } else {
                     run('rmdir /data/web/public');
-                    run('ln -s {{release_path}}/{{public_folder}} /data/web/public');
+                    run('ln -s {{current_path}}/{{public_folder}} /data/web/public');
                 }
             } else {
                 run('ln -s {{current_path}}/{{public_folder}} /data/web/public');
